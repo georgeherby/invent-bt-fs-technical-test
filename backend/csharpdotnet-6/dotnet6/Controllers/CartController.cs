be6ae95a8dbc2b27e3d0de0c65dac7483f6decb0
JamesArmerCI <112471624+JamesArmerCI@users.noreply.github.com>
2022-09-01 10:06:59 +0100

    Added dotnet 6 version

---
+++ backend/csharpdotnet-6/dotnet6/Controllers/CartController.cs
@@ -0,0 +1,54 @@
+﻿using dotnet6.Interfaces;
+using dotnet6.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet6.Controllers;
+
+[ApiController]
+[Produces("application/json")]
+[Route("[controller]")]
+public class CartController : ControllerBase
+{
+    private readonly ICartService _cartService;
+
+    public CartController(ICartService cartService)
+    {
+        _cartService = cartService;
+    }
+
+    [Route("Items")]
+    [HttpGet]
+    public ActionResult<ItemsResponse> GetShopItems()
+    {
+        return Ok(new ItemsResponse(
+            new List<Product>()
+            {
+                    new Product("Apple", 60),
+                    new Product("Orange", 25)
+            }
+            )
+        );
+    }
+
+    [Route("Add")]
+    [HttpPost]
+    public ActionResult<double> AddItemsToShoppingCart(string[] items)
+    {
+        throw new NotImplementedException();
+    }
+
+    [Route("Total")]
+    [HttpGet]
+    public ActionResult<double> GetTotalPrice()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Route("Remove")]
+    [HttpDelete]
+    public ActionResult<double> RemoveItemFromShoppingCart(string[] items)
+    {
+        throw new NotImplementedException();
+    }
+}
+
---
+++ backend/csharpdotnet-6/dotnet6_tests/CartControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using dotnet6.Controllers;
+using dotnet6.Interfaces;
+using dotnet6.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet6_tests;
+
+public class CartControllerTests
+{
+    [Fact]
+    public void ShouldReturnListOfItems()
+    {
+        var mockCartService = new Mock<ICartService>();
+        var controller = new CartController(mockCartService.Object);
+
+        var result = controller.GetShopItems();
+
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var itemsResponse = Assert.IsType<ItemsResponse>(okResult.Value);
+        var productList = itemsResponse.ProductList;
+        Assert.Equal("Apple", productList[0].ItemName);
+        Assert.Equal(25, productList[1].Price);
+    }
+}
